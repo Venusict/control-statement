8522003d8b9726a2eb351c14889a4a1b7a76d89f
Mars Ryzen <quangdat.ict@gmail.com>
2021-10-07 21:16:01 +0700

    I added a new line at first project

--- firstproject/src/com/viettel/Main.java
+++ firstproject/src/com/viettel/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             System.out.println("ja" + i);
+            System.out.println("ja" + i);
         }
     }
 }
